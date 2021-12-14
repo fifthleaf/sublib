675d2096f4e74c83b770d448320cd7fd5ed400d5
Dawid Jach <jach.developer@protonmail.com>
2021-12-14 15:42:56 +0100

    Restructuring package

--- test_sublib.py
+++ test_sublib.py
@@ -2,7 +2,7 @@ import unittest as ut
 from unittest.mock import patch, mock_open
 import datetime
 
-import sublib.sublib as sbl
+import sublib as sbl
 
 
 class DetectTest(ut.TestCase):
@@ -14,7 +14,7 @@ class DetectTest(ut.TestCase):
             self.assertNotEqual("srt", sbl.detect(mock_file, "utf-8"))
             self.assertNotEqual("sub", sbl.detect(mock_file, "utf-8"))
             self.assertNotEqual("tmp", sbl.detect(mock_file, "utf-8"))
-            self.assertNotEqual(False, sbl.detect(mock_file, "utf-8"))
+            self.assertNotEqual("undefined", sbl.detect(mock_file, "utf-8"))
 
     def test_srt(self):
         test_data = "1\n00:01:00,060 --> 00:01:03,105\nLine 01\nLine 02\n\n2\n00:01:03,272 --> 00:01:05,440\nLine 03\nLine 04\n\n3\n00:01:05,607 --> 00:01:06,775\nLine 04\n\n"
@@ -23,7 +23,7 @@ class DetectTest(ut.TestCase):
             self.assertEqual("srt", sbl.detect(mock_file, "utf-8"))
             self.assertNotEqual("sub", sbl.detect(mock_file, "utf-8"))
             self.assertNotEqual("tmp", sbl.detect(mock_file, "utf-8"))
-            self.assertNotEqual(False, sbl.detect(mock_file, "utf-8"))
+            self.assertNotEqual("undefined", sbl.detect(mock_file, "utf-8"))
 
     def test_sub(self):
         test_data = "{1440}{1513}Line 01|Line 02\n{1517}{1569}Line 03|Line 04\n{1573}{1601}Line 04\n"
@@ -32,7 +32,7 @@ class DetectTest(ut.TestCase):
             self.assertNotEqual("srt", sbl.detect(mock_file, "utf-8"))
             self.assertEqual("sub", sbl.detect(mock_file, "utf-8"))
             self.assertNotEqual("tmp", sbl.detect(mock_file, "utf-8"))
-            self.assertNotEqual(False, sbl.detect(mock_file, "utf-8"))
+            self.assertNotEqual("undefined", sbl.detect(mock_file, "utf-8"))
 
     def test_tmp(self):
         test_data = "00:01:00:Line 01|Line 02\n00:01:03:Line 03|Line 04\n00:01:05:Line 04"
@@ -41,7 +41,7 @@ class DetectTest(ut.TestCase):
             self.assertNotEqual("srt", sbl.detect(mock_file, "utf-8"))
             self.assertNotEqual("sub", sbl.detect(mock_file, "utf-8"))
             self.assertEqual("tmp", sbl.detect(mock_file, "utf-8"))
-            self.assertNotEqual(False, sbl.detect(mock_file, "utf-8"))
+            self.assertNotEqual("undefined", sbl.detect(mock_file, "utf-8"))
 
     def test_false(self):
         test_data = "Line 01\nLine 02\nLine 03"
@@ -50,7 +50,7 @@ class DetectTest(ut.TestCase):
             self.assertNotEqual("srt", sbl.detect(mock_file, "utf-8"))
             self.assertNotEqual("sub", sbl.detect(mock_file, "utf-8"))
             self.assertNotEqual("tmp", sbl.detect(mock_file, "utf-8"))
-            self.assertEqual(False, sbl.detect(mock_file, "utf-8"))
+            self.assertEqual("undefined", sbl.detect(mock_file, "utf-8"))
 
 
 class MPlayer2Test(ut.TestCase):
@@ -66,11 +66,11 @@ class MPlayer2Test(ut.TestCase):
         format_lines = "[601][631] Line 01|Line 02\n[633][654] Line 03|Line 04\n[656][668] Line 04\n"
         with patch("builtins.open", mock_open(read_data=format_lines)) as mock_file:
             with open(mock_file, "rt", encoding="utf-8", errors="ignore") as path_file:
-                self.assertEqual(self.general_lines, sbl.from_mpl(path_file, "file.txt"))
+                self.assertEqual(self.general_lines, sbl.from_mpl(path_file))
 
     def test_to(self):
         format_lines = ["[601][631] Line 01|Line 02", "[633][654] Line 03|Line 04", "[656][668] Line 04"]
-        self.assertEqual(format_lines, sbl.to_mpl(self.general_lines, "file.txt"))
+        self.assertEqual(format_lines, sbl.to_mpl(self.general_lines))
 
 
 class SubRipTest(ut.TestCase):
@@ -86,11 +86,11 @@ class SubRipTest(ut.TestCase):
         format_lines = "1\n00:01:00,060 --> 00:01:03,105\nLine 01\nLine 02\n\n2\n00:01:03,272 --> 00:01:05,440\nLine 03\nLine 04\n\n3\n00:01:05,607 --> 00:01:06,775\nLine 04\n\n"
         with patch("builtins.open", mock_open(read_data=format_lines)) as mock_file:
             with open(mock_file, "rt", encoding="utf-8", errors="ignore") as path_file:
-                self.assertEqual(self.general_lines, sbl.from_srt(path_file, "file.txt"))
+                self.assertEqual(self.general_lines, sbl.from_srt(path_file))
 
     def test_to(self):
         format_lines = ["1\n00:01:00,060 --> 00:01:03,105\nLine 01\nLine 02\n", "2\n00:01:03,272 --> 00:01:05,440\nLine 03\nLine 04\n", "3\n00:01:05,607 --> 00:01:06,775\nLine 04\n"]
-        self.assertEqual(format_lines, sbl.to_srt(self.general_lines, "file.txt"))
+        self.assertEqual(format_lines, sbl.to_srt(self.general_lines))
 
 
 class MicroDVDTest(ut.TestCase):
@@ -106,11 +106,11 @@ class MicroDVDTest(ut.TestCase):
         format_lines = "{1440}{1513}Line 01|Line 02\n{1517}{1569}Line 03|Line 04\n{1573}{1601}Line 04\n"
         with patch("builtins.open", mock_open(read_data=format_lines)) as mock_file:
             with open(mock_file, "rt", encoding="utf-8", errors="ignore") as path_file:
-                self.assertEqual(self.general_lines, sbl.from_sub(path_file, "file.txt"))
+                self.assertEqual(self.general_lines, sbl.from_sub(path_file))
 
     def test_to(self):
         format_lines = ["{1440}{1513}Line 01|Line 02", "{1517}{1569}Line 03|Line 04", "{1573}{1601}Line 04"]
-        self.assertEqual(format_lines, sbl.to_sub(self.general_lines, "file.txt"))
+        self.assertEqual(format_lines, sbl.to_sub(self.general_lines))
 
 
 class TMPlayerTest(ut.TestCase):
@@ -126,11 +126,11 @@ class TMPlayerTest(ut.TestCase):
         format_lines = "00:01:00:Line 01|Line 02\n00:01:03:Line 03|Line 04\n00:01:05:Line 04"
         with patch("builtins.open", mock_open(read_data=format_lines)) as mock_file:
             with open(mock_file, "rt", encoding="utf-8", errors="ignore") as path_file:
-                self.assertEqual(self.general_lines, sbl.from_tmp(path_file, "file.txt"))
+                self.assertEqual(self.general_lines, sbl.from_tmp(path_file))
 
     def test_to(self):
         format_lines = ["00:01:00:Line 01|Line 02", "00:01:03:Line 03|Line 04", "00:01:05:Line 04"]
-        self.assertEqual(format_lines, sbl.to_tmp(self.general_lines, "file.txt"))
+        self.assertEqual(format_lines, sbl.to_tmp(self.general_lines))
 
 
 if __name__ == '__main__':
