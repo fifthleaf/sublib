fc645b9aac5ec844406d56c3f560f23d13b36ad9
Dawid Jach <jach.developer@protonmail.com>
2021-12-15 17:46:11 +0100

    Add doctrings and comments to classes

--- sublib/sublib.py
+++ sublib/sublib.py
@@ -221,8 +221,18 @@ def to_tmp(lines):
 
 
 class Subtitle:
+    """A class to represent subtitle file in general. The specific format classes inherit from it.
+    Attributes:
+        path (str): Absolute or relative path to subtitle file that will be read
+        encoding (str): Valid encoding that will be use to open a subtitle file
+    """
 
     def __init__(self, path=None, encoding=None):
+        """Construct classes attributes.
+        Parameters:
+            path (str): Absolute or relative path to subtitle file that will be read
+            encoding (str): Valid encoding that will be use to open a subtitle file
+        """
         self.path = path
         self.encoding = encoding
         if self.path is not None and \
@@ -238,46 +248,70 @@ class Subtitle:
             self.content = None
 
     def __str__(self):
+        """Specifies how str() is displayed: class_name("path", "encoding")"""
         return f'{self.__class__.__name__}("{self.path}", "{self.encoding}")'
 
     def __repr__(self):
+        """Specifies how repr() is displayed: class_name(path="path", encoding="encoding")"""
         return f'{self.__class__.__name__}(path="{self.path}", encoding="{self.encoding}")'
 
     def __bool__(self):
+        """Specifies what bool() should return: True if self.content is not None"""
         return False if self.content is None else True
 
     def __eq__(self, other):
+        """Specifies when subtitle calsses are equal: True if self.content value of both is equal"""
         return True if self.content == other.content else False
 
     def __len__(self):
+        """Specifies what len() should return: Number of lines in file"""
         return len(self.content)
 
     def __contains__(self, item):
+        """Specifies what "in" should do: Search for match in every line"""
         for line in self.content:
             if line.count(item) > 0:
                 return True
 
     def __iter__(self):
+        """Specifies what iter() should do: Set line index to 0"""
         self._i = 0
         return self
 
     def __next__(self):
+        """Specifies what next() should return: Line of file at a specific index"""
         line = self.content[self._i]
         self._i += 1
         return line
 
 
 class MPlayer2(Subtitle):
+    """A class to represent MPlayer2 subtitle file.
+    Attributes:
+        path (str): Absolute or relative path to subtitle file that will be read
+        encoding (str): Valid encoding that will be use to open a subtitle file
+    """
 
     def __init__(self, path=None, encoding=None):
+        """Construct classes attributes.
+        Parameters:
+            path (str): Absolute or relative path to subtitle file that will be read
+            encoding (str): Valid encoding that will be use to open a subtitle file
+        """
         super().__init__(path, encoding)
         self.format = r"\\[[0-9]+\\]\\[[0-9]+\\] .*\n"
 
     def get_general_format(self):
+        """Convert MPlayer2 formatted lines to general list
+        Parameters:
+            None
+        Returns:
+            list: lines one by one in general format
+        """
         lines = [line.split("]", 2) for line in (line.rstrip("\n") for line in self.content)]
         for line in lines:
             line[0] = dt.timedelta(
-                seconds=round(float(line[0].replace("[", "")) / 10.0, 1)
+                seconds=round(float(line[0].replace("[", "")) / 10.0, 1)    # MPL use as time: sec * 10
             )
             line[1] = dt.timedelta(
                 seconds=round(float(line[1].replace("[", "")) / 10.0, 1)
@@ -286,19 +320,41 @@ class MPlayer2(Subtitle):
         return lines
 
     def set_from_general_format(self, lines):
+        """Convert general list to MPlayer2 foramt
+        Parameters:
+            lines (list): Lines in general formatt that will be converted
+        Action:
+            It sets self.content from provided list of lines.
+        """
         for line in lines:
-            line[0] = round(line[0].total_seconds() * 10)
+            line[0] = round(line[0].total_seconds() * 10)    # MPL use as time: sec * 10
             line[1] = round(line[1].total_seconds() * 10)
         self.content = [f"[{line[0]}][{line[1]}] {line[2]}" for line in lines]
 
 
 class SubRip(Subtitle):
+    """A class to represent SubRip subtitle file.
+    Attributes:
+        path (str): Absolute or relative path to subtitle file that will be read
+        encoding (str): Valid encoding that will be use to open a subtitle file
+    """
 
     def __init__(self, path=None, encoding=None):
+        """Construct classes attributes.
+        Parameters:
+            path (str): Absolute or relative path to subtitle file that will be read
+            encoding (str): Valid encoding that will be use to open a subtitle file
+        """
         super().__init__(path, encoding)
         self.format = r"[0-9]+\n[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}\n*\n"
 
     def get_general_format(self):
+        """Convert SubRip formatted lines to general list
+        Parameters:
+            None
+        Returns:
+            list: lines one by one in general format
+        """
         lines, temp = [], []
         for line in self.content:
             if line != "\n":
@@ -329,11 +385,17 @@ class SubRip(Subtitle):
         return lines
 
     def set_from_general_format(self, lines):
+        """Convert general list to SubRip foramt
+        Parameters:
+            lines (list): Lines in general formatt that will be converted
+        Action:
+            It sets self.content from provided list of lines.
+        """
         for line in lines:
             line[0] = str(line[0])
             line[1] = str(line[1])
             if len(line[0]) == 7:
-                line[0] = line[0] + "." + "".zfill(6)
+                line[0] = line[0] + "." + "".zfill(6)    # If no microsecons, fill with zeros
             if len(line[1]) == 7:
                 line[1] = line[1] + "." + "".zfill(6)
             line[0] = dt.datetime.strptime(line[0], "%H:%M:%S.%f")
@@ -348,16 +410,32 @@ class SubRip(Subtitle):
 
 
 class MicroDVD(Subtitle):
+    """A class to represent MicroDVD subtitle file.
+    Attributes:
+        path (str): Absolute or relative path to subtitle file that will be read
+        encoding (str): Valid encoding that will be use to open a subtitle file
+    """
 
     def __init__(self, path=None, encoding=None):
+        """Construct classes attributes.
+        Parameters:
+            path (str): Absolute or relative path to subtitle file that will be read
+            encoding (str): Valid encoding that will be use to open a subtitle file
+        """
         super().__init__(path, encoding)
         self.format = r"{[0-9]+}{[0-9]+}.*\n"
 
     def get_general_format(self):
+        """Convert MicroDVD formatted lines to general list
+        Parameters:
+            None
+        Returns:
+            list: lines one by one in general format
+        """
         lines = [line.split("}", 2) for line in (line.rstrip("\n") for line in self.content)]
         for line in lines:
             line[0] = dt.timedelta(
-                seconds=round(float(line[0].replace("{", "")) / 23.976, 3)
+                seconds=round(float(line[0].replace("{", "")) / 23.976, 3)    # SUB use frames as time
             )
             line[1] = dt.timedelta(
                 seconds=round(float(line[1].replace("{", "")) / 23.976, 3)
@@ -367,19 +445,41 @@ class MicroDVD(Subtitle):
         return lines
 
     def set_from_general_format(self, lines):
+        """Convert general list to MicroDVD foramt
+        Parameters:
+            lines (list): Lines in general formatt that will be converted
+        Action:
+            It sets self.content from provided list of lines.
+        """
         for line in lines:
-            line[0] = round(line[0].total_seconds() * 23.976)
+            line[0] = round(line[0].total_seconds() * 23.976)    # SUB use frames as time
             line[1] = round(line[1].total_seconds() * 23.976)
         self.content = [f"{{{line[0]}}}{{{line[1]}}}{line[2]}" for line in lines]
 
 
 class TMPlayer(Subtitle):
+    """A class to represent TMPlayer subtitle file.
+    Attributes:
+        path (str): Absolute or relative path to subtitle file that will be read
+        encoding (str): Valid encoding that will be use to open a subtitle file
+    """
 
     def __init__(self, path=None, encoding=None):
+        """Construct classes attributes.
+        Parameters:
+            path (str): Absolute or relative path to subtitle file that will be read
+            encoding (str): Valid encoding that will be use to open a subtitle file
+        """
         super().__init__(path, encoding)
         self.format = r"[0-9]+:[0-9]+:[0-9]+:.*\n"
 
     def get_general_format(self):
+        """Convert TMPlayer formatted lines to general list
+        Parameters:
+            None
+        Returns:
+            list: lines one by one in general format
+        """
         lines = [line.split(":", 3) for line in (line.rstrip("\n") for line in self.content)]
         lines = [[f"{line[0]}:{line[1]}:{line[2]}", f"{line[0]}:{line[1]}:{line[2]}", line[3]]
                  for line in lines]
@@ -401,10 +501,16 @@ class TMPlayer(Subtitle):
         return lines
 
     def set_from_general_format(self, lines):
+        """Convert general list to TMPlayer foramt
+        Parameters:
+            lines (list): Lines in general formatt that will be converted
+        Action:
+            It sets self.content from provided list of lines.
+        """
         for line in lines:
             line[0] = str(line[0])
             if len(line[0]) == 7:
-                line[0] = line[0] + "." + "".zfill(6)   # If no microsecons, fill with zeros
+                line[0] = line[0] + "." + "".zfill(6)    # If no microsecons, fill with zeros
             line[0] = dt.datetime.strptime(line[0], "%H:%M:%S.%f")
             line[0] = line[0].strftime("%H:%M:%S.%f")
             line[0] = line[0][:len(line[0]) - 7]
