7acc84fb2a501ff6fc0ab3ae3d53f6640f0e59f3
Dawid Jach <jach.developer@protonmail.com>
2021-12-15 17:55:49 +0100

    Update sublib/__init__.py

--- sublib/__init__.py
+++ sublib/__init__.py
@@ -1,1 +1,1 @@
-from sublib.sublib import detect, from_mpl, to_mpl, from_srt, to_srt, from_sub, to_sub, from_tmp, to_tmp
+from sublib.sublib import detect, Subtitle, MPlayer2, SubRip, MicroDVD, TMPlayer
